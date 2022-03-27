8c793d8a7cee777c81f139d1f51024b7454d7511
Binayak Thakur <thakurganesh919@gmail.com>
2022-03-27 16:16:41 +0530

    Passing testcases defined for first testcase

--- String_Calculator_TDD_Kata.java
+++ String_Calculator_TDD_Kata.java
@@ -1,10 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class String_Calculator_TDD_Kata {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(add(sc.nextLine()));
+
+        sc.close();
+    }
+
+    static int add(String numbers) {
+        // Conditions
+        try {
+            String delimiter = ",";
+            int[] numbersArray = Arrays.stream(numbers.split(delimiter)).mapToInt(Integer::parseInt).toArray();
+            return Arrays.stream(numbersArray).sum();
+        } catch (Exception e) {
+            if (numbers.equals("")) {
+                return 0;
+            } else {
+                throw new IllegalArgumentException("Invalid input");
+            }
+        }
 
     }
 
-    int add(String numbers) {
-        return -1;
-    }
 }
